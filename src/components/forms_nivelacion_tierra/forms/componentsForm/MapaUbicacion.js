5294d5f2895c69d86f7222326fd1e297ec952434
gomez50057 <gomez.50057@gmail.com>
2025-04-24 04:15:19 -0600

    Icon

--- src/components/forms_nivelacion_tierra/forms/componentsForm/MapaUbicacion.js
+++ src/components/forms_nivelacion_tierra/forms/componentsForm/MapaUbicacion.js
@@ -7,9 +7,9 @@ import { Hgo_Info } from '../../../landing/maps/Hgo';
 
 delete L.Icon.Default.prototype._getIconUrl;
 L.Icon.Default.mergeOptions({
-  iconRetinaUrl: '/leaflet/marker-icon-2x.png',
-  iconUrl: '/leaflet/marker-icon.png',
-  shadowUrl: '/leaflet/marker-shadow.png',
+  iconRetinaUrl: '/img/forms nivelacion tierra/iconos/pin.png',
+  iconUrl: '/img/forms nivelacion tierra/iconos/pin.png',
+  shadowUrl: '',
 });
 
 const LocationPicker = ({ setFieldValue }) => {
