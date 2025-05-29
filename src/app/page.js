540ac6b9c8c76aaeb974c8276c5c23bf651c2051
gomez50057 <gomez.50057@gmail.com>
2025-05-29 15:49:42 -0600

    orden en paguina inicial mapa haci abajo

--- src/app/page.js
+++ src/app/page.js
@@ -4,8 +4,6 @@ import JustificationSection from "@/components/landing/about/About";
 // import MapSection from "@/components/landing/maps/ProjectMap";
 import MapSection from "@/components/landing/MapSection";
 import InfoSwiper from "@/components/landing/InfoSwiper";
-
-
 import PublicKnowledgeSection from "@/components/landing/PublicKnowledgeSection";
 
 export default function HomePage() {
@@ -14,48 +12,9 @@ export default function HomePage() {
       <HeroSection />
       <AboutSection />
       <JustificationSection />
-      <MapSection />
       <InfoSwiper />
       <PublicKnowledgeSection />
+      <MapSection />
     </main>
   );
 }
-
-
-// "use client";
-
-// import dynamic from 'next/dynamic';
-// import HeroSection from "@/components/landing/Hero";
-// import AboutSection from "@/components/landing/About";
-// import JustificationSection from "@/components/landing/about/About";
-// import MapSection from "@/components/landing/maps/ProjectMap";
-// import PublicKnowledgeSection from "@/components/landing/PublicKnowledgeSection";
-// import TrainingSection from "@/components/landing/TrainingSection";
-
-// // Importa WaterWave de forma dinámica sin SSR
-// const WaterWave = dynamic(() => import('react-water-wave'), { ssr: false });
-
-// export default function HomePage() {
-//   return (
-//     <main>
-//       <HeroSection />
-//       <WaterWave 
-//         // imageUrl="/img/background.jpg"
-//         dropRadius={20}
-//         perturbance={0.02}
-//         resolution={256}
-//         interactive={true}
-//       >
-//         {() => (
-//           <>
-//             <AboutSection />
-//             <JustificationSection />
-//           </>
-//         )}
-//       </WaterWave>
-//       <MapSection />
-//       <PublicKnowledgeSection />
-//       <TrainingSection />
-//     </main>
-//   );
-// }
