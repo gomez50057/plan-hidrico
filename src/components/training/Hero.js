8e9232f215ad42dd53ff9bfc4cc88b6a68dc4b61
gomez50057 <gomez.50057@gmail.com>
2025-06-02 15:42:38 -0600

    hero

--- src/components/training/Hero.js
+++ src/components/training/Hero.js
@@ -5,12 +5,23 @@ const imgBasePath = "/img/training/";
 
 
 export default function Hero() {
-    return (
+  return (
     <section>
       <div className={styles.contentHeader}>
-        <div className={`${styles.headerImg}`}>
-          <img src={`${imgBasePath}hero.png`} alt="portada de Ecos del territorio hídrico" className={styles.floatingImg} />
+       
+        <div className={styles.headerTxt}>
+          <div className={styles.headerImg02}>
+            <img src={`${imgBasePath}hero 02.png`} alt="portada de Ecos del territorio hídrico" className={styles.floatingImg} />
+          </div>
+          <div className={styles.headerImg03}>
+            <img src={`${imgBasePath}hero 03.png`} alt="portada de Ecos del territorio hídrico" className={styles.floatingImg} />
+          </div>
         </div>
+
+         <div className={styles.headerImg01}>
+          <img src={`${imgBasePath}hero 01.png`} alt="portada de Ecos del territorio hídrico" className={styles.floatingImg} />
+        </div>
+
       </div>
     </section>
   );
